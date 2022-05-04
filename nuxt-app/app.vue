a84ce0d7faece5d78a620722b5a33ccf28a0867d
ilg2005 <igoldfarb10@gmail.com>
2022-05-04 09:23:01 +0300

    favicon

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
+    <Head>
+      <Title>{{ title }}</Title>
+      <Meta name="description" :content="title" />
+      <Link rel="shortcut icon" href="/icon_64x64.a3b4ce.png" as="favicon" />
+    </Head>
     <TheNavbar />
   </div>
 </template>
-<script>export default {
-  components: {}
-}
+<script setup>
+const title = 'Task Manager';
 </script>
