e819f8555a41b940b9f6a535d5d6b4ffd6cb8678
ilg2005 <igoldfarb10@gmail.com>
2022-05-07 15:39:45 +0300

    текущее состояние

--- nuxt-app/pages/task/[id].vue
+++ nuxt-app/pages/task/[id].vue
@@ -1,20 +1,23 @@
 <template>
-  <div class="card" v-if="selectedTask">
-    <h2>{{ selectedTask.title }}</h2>
-    <p><strong>Статус</strong>:
-      <AppStatus :type="selectedTask.status" :key="statusChanged"/>
-    </p>
-    <p><strong>Дедлайн</strong>: {{ new Date(selectedTask.deadline).toLocaleDateString() }}</p>
-    <p><strong>Описание</strong>: {{ selectedTask.description }}</p>
-    <div>
-      <button class="btn" @click="changeStatus('pending')">Взять в работу</button>
-      <button class="btn primary" @click="changeStatus('done')">Завершить</button>
-      <button class="btn danger" @click="changeStatus('cancelled')">Отменить</button>
+  <AppLoader v-if="isLoading"/>
+  <div v-else>
+    <div class="card" v-if="selectedTask">
+      <h2>{{ selectedTask.title }}</h2>
+      <p><strong>Статус</strong>:
+        <AppStatus :type="selectedTask.status" :key="statusChanged"/>
+      </p>
+      <p><strong>Дедлайн</strong>: {{ new Date(selectedTask.deadline).toLocaleDateString() }}</p>
+      <p><strong>Описание</strong>: {{ selectedTask.description }}</p>
+      <div>
+        <button class="btn" @click="changeStatus('pending')">Взять в работу</button>
+        <button class="btn primary" @click="changeStatus('done')">Завершить</button>
+        <button class="btn danger" @click="changeStatus('cancelled')">Отменить</button>
+      </div>
     </div>
+    <h3 class="text-white center" v-else>
+      Задачи с id = <strong>{{ id }}</strong> нет.
+    </h3>
   </div>
-  <h3 class="text-white center" v-else>
-    Задачи с id = <strong>{{ id }}</strong> нет.
-  </h3>
 </template>
 
 <script setup>
@@ -24,8 +27,14 @@ import {useRoute} from "nuxt/app";
 const route = useRoute();
 const id = +route.params.id;
 
-const tasks = computed(() => JSON.parse(localStorage.getItem('tasks')));
-const selectedTask = tasks.value.find(task => task.id === id);
+const tasks = ref();
+const selectedTask = ref();
+
+const isLoading = computed(() => {
+  tasks.value = JSON.parse(localStorage.getItem('tasks'));
+  selectedTask.value = tasks.value.find(task => task.id === id);
+  return typeof window === 'undefined';
+});
 
 const statusChanged = ref(0);
 
