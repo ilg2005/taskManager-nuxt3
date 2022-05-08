82e692f000318706577ff8de4fe6aa7ad78f8587
ilg2005 <igoldfarb10@gmail.com>
2022-05-08 11:04:46 +0300

    текущее состояние

---
+++ nuxt-app/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1 class="text-white center" v-if="!tasks.length">Задач пока нет</h1>
+    <div v-else>
+      <h3 class="text-white">Всего {{ }} задач: {{ tasks.length }}</h3>
+      <task-card v-for="task of tasks"
+                 :key="task.id"
+                 :task="task"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+defineProps(['tasks']);
+
+</script>
+
+<style scoped>
+
+</style>
--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -1,16 +1,16 @@
 <template>
-  <h1 class="text-white center" v-if="!tasks.length">Задач пока нет</h1>
-  <div v-else>
-    <h3 class="text-white">Всего {{  }} задач: {{ tasks.length }}</h3>
-    <task-card v-for="task of tasks"
-                    :key="task.id"
-                    :task="task"
-    />
-  </div>
+  <Tasks :tasks="tasks"/>
 </template>
 
 <script setup>
 
-const tasks = computed(() => JSON.parse(localStorage.getItem('tasks')) ?? []);
+const tasks = ref();
+const tasksFromLS= computed(() => {
+  if (typeof window !== 'undefined') {
+    return JSON.parse(localStorage.getItem('tasks')) ?? [];
+  }
+});
+
+tasks.value = tasksFromLS.value;
 
 </script>
